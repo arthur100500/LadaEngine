114b4d37321cfe1edacce50a719646e90e4710ac
Arthur Alekseev <57834711+arthur100500@users.noreply.github.com>
2023-04-08 03:56:51 +0300

    Add doc to IRenderable

--- src/LadaEngine/Engine/Base/IRenderable.cs
+++ src/LadaEngine/Engine/Base/IRenderable.cs
@@ -2,7 +2,14 @@
 
 namespace LadaEngine.Engine.Base;
 
+/// <summary>
+/// Interface that describes renerable stuff
+/// </summary>
 public interface IRenderable
 {
+    /// <summary>
+    /// Renders the objects relatively to the camera
+    /// </summary>
+    /// <param name="camera"></param>
     void Render(Camera camera);
 }
